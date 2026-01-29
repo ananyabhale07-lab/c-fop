49b88ff1a2df8c081ac8bb7a71f65f94bd5a9038
atharvb13 <ananyabhale07@gmail.com>
2026-01-29 18:27:26 +0530

    Update basic operations.c

--- basic operations.c
+++ basic operations.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
-void main() {
-    int a,b;
-    printf("Enter two integers: ");
-    scanf("%d %d", &a, &b);
-    printf("Sum of %d and %d is %d\n", a, b, a+b);
+void main(){
+    float a,b;
+    printf("Enter two numbers: ");
+    scanf("%f %f",&a,&b);
+    printf("Sum: %f\n",a+b);
+    printf("Difference: %f\n",a-b);
+    printf("Product: %f\n",a*b);
+    printf("Division: %f\n",a/b);
 }
