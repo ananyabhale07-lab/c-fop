68993dc9b3b19bb9b9f0414c2c67f28a466b655b
ananyabhale07-lab <ananyabhale07@gmail.com>
2026-02-28 00:30:24 +0530

    reverse of a number

--- Assignment13_reversenumber.c
+++ Assignment13_reversenumber.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+void main(){
+  int num,remainder,reverse=0;  
+  printf("Enter a number");
+  scanf("%d",&num);
+while(num!=0){
+remainder=num%10;
+reverse=reverse*10+remainder;
+num=num/10;
+}
+printf("Reversed number is:%d",reverse);
+}
 
