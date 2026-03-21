9a025a0a2e2c70486eafcad32f65f2480d98e81c
ananyabhale07-lab <ananyabhale07@gmail.com>
2026-03-21 20:29:14 +0530

    Update stringop.c

--- stringop.c
+++ stringop.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+char arr[10];
+printf("enter the string");
+scanf("%s",arr);
+//using strlen to calculate the length of the string
+ printf("Length = %lu", strlen(arr));
+}
+
+
 
